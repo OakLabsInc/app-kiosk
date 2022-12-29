a56b2d5802e749cac428dcad3f3b4d29cc4676a7
Richard Joffray <Richard.Joffray@verifone.com>
2022-12-29 03:17:09 -0800

    fixed jquery

--- app-kiosk/main.js
+++ app-kiosk/main.js
@@ -1,6 +1,21 @@
 const { screen, app, BrowserWindow } = require("electron");
 const args = require('minimist')(process.argv)
 const display_id = args.displayid || 0
+const { join } = require('path')
+const express = require('express')
+
+
+const exapp = express()
+http = require('http'),
+  httpServer = http.Server(exapp);
+
+let publicPath = join(__dirname, 'src/public')
+
+exapp.use(express.static(publicPath))
+exapp.listen(80)
+exapp.get('/', function(req, res) {
+  res.sendFile(join(__dirname, 'src','public', 'index.html'));
+});
 
 
 function createWindow() {
@@ -20,7 +35,7 @@ function createWindow() {
     }
   });
 
-  win.loadFile("src/public/index.html");
+  win.loadURL('http://localhost:80');
 }
 
 app.whenReady().then(() => {
