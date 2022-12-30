f45628de14d44350e3b525394a14f0ed5646f176
Richard Joffray <Richard.Joffray@verifone.com>
2022-12-30 13:50:33 -0800

    added payment test module

---
+++ app-kiosk/test/payment.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process')
+console.log(process.env)
+let data = `{"cart":{"total":"40.4","tax":"3.434","taxRate":"0.085","grandTotal":"43.833999999999996"},"terminalIp":"${process.env.TERMINAL_IP}"}`
+
+async function sendCart(payload) {
+
+	await exec(`curl --header "Content-Type: application/json" --request POST --data '${data}' localhost:${process.env.PAYMENT_PORT}`,(err, stdout, stderr) => {
+    if(err) {
+      // node couldn't execute the command
+      console.log(`Error: ${err}`);
+      return;
+    }
+    // the *entire* stdout and stderr (buffered)
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
+  })
+}
